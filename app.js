b51d2075536ee2d9ca79df2b806802124ca8d837
AnnaKhiz <kukurika.anna@gmail.com>
2024-03-20 13:27:15 +0200

    create index pug, base styles

--- app.js
+++ app.js
@@ -1,8 +1,16 @@
 require('dotenv').config();
+const sass = require('./utils/sass.js')
+
+
 const express = require('express');
-const server = express()
+const server = express();
 
 const { PORT } = require('./config/default');
 
 server.listen(PORT, () => console.log(`Server started on port ${PORT}`));
+server.use('/public', express.static('static'));
+server.set('view engine', 'pug');
 
+server.get('/', (req, res) => {
+	res.render('index', {})
+})
---
+++ utils/sass.js
@@ -0,0 +1,24 @@
+const sass = require('node-sass');
+const fs = require('fs');
+const path = require('path');
+
+
+
+sass.render({
+	file: path.join('./', 'src', 'sass', 'style.sass'),
+	outFile: path.join('./', 'static', 'style.css')
+}, function(err, result) {
+	if (!err) {
+		fs.writeFile(path.join('./', 'static', 'style.css'), result.css, function(err) {
+			if (!err) {
+				console.log('Sass file compiled successfully!');
+			} else {
+				console.error('Error writing CSS file:', err);
+			}
+		});
+	} else {
+		console.error('Error compiling Sass file:', err);
+	}
+});
+
+module.exports = sass
