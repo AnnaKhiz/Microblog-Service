e5974f1f70c409efa96c4f8ec13298f013a78c82
evhenious <evhenious@gmail.com>
2024-03-31 14:10:06 +0300

    basic linter rules

---
+++ .eslintrc.js
@@ -0,0 +1,26 @@
+module.exports = {
+  env: {
+    node: true,
+    commonjs: true,
+    es2021: true,
+  },
+  extends: 'eslint:recommended',
+  overrides: [
+    {
+      env: {
+        node: true,
+      },
+      files: ['.eslintrc.{js,cjs}'],
+      parserOptions: {
+        sourceType: 'module',
+      },
+    },
+  ],
+  parserOptions: {
+    ecmaVersion: 'latest',
+  },
+  ignorePatterns: ['/static/*'], // linting only node part now
+  rules: {
+    semi: ['warn', 'always'],
+  },
+};
