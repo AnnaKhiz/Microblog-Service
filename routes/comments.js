a0bf9d500c3ee67417eb8312c7a96fc46c41f905
evhenious <evhenious@gmail.com>
2024-04-05 08:47:51 +0300

    fixed var name, better wording

--- routes/comments.js
+++ routes/comments.js
@@ -2,10 +2,10 @@ const router = require('express').Router();
 const { addComment, deleteComment } = require('../services/apiComments.js');
 const { parserJwt } = require('../middleware/auth');
 const { protectedRoute } = require('../middleware/route');
-const { validation } = require('../middleware/validation');
+const { validateCommentId } = require('../middleware/validation');
 
 router.post('/', parserJwt, protectedRoute(['user'], '/auth/login'), addComment);
 
-router.delete('/:id', parserJwt, protectedRoute(['user', 'admin'], '/auth/login'), validation, deleteComment);
+router.delete('/:id', parserJwt, protectedRoute(['user', 'admin'], '/auth/login'), validateCommentId, deleteComment);
 
 module.exports = { router };
